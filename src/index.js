9eec7191d7d6d5d405855e977b1d5fc179dc7540
lg4tin <lg4tin@gmail.com>
2023-09-25 22:29:16 -0400

    Create function to add to nav buttons

--- src/index.js
+++ src/index.js
@@ -9,10 +9,27 @@ const menuButton = document.querySelector('.menu');
 const contactButton = document.querySelector('.contact');
 
 homeButton.addEventListener('click', pageLoad);
+homeButton.addEventListener('click', () => {
+  menuSwitch(menuButton, contactButton, homeButton);
+})
 menuButton.addEventListener('click', displayMenu);
+menuButton.addEventListener('click', () => {
+  menuSwitch(homeButton, contactButton, menuButton);
+})
 contactButton.addEventListener('click', displayContactPage);
+contactButton.addEventListener('click', () => {
+  menuSwitch(menuButton, homeButton, contactButton);
+})
 
 console.log('Hiii');
 
 
-
+function menuSwitch(btn1, btn2, btn3) {
+  if (!btn1.classList.contains('clicked') && !btn2.classList.contains('clicked')) {
+    btn3.classList.add('clicked');
+  } else if (btn1.classList.contains('clicked') || btn2classList.contains('clicked')) {
+    btn1.classList.remove('clicked');
+    btn2.classList.remove('clicked');
+    btn3.classList.add('clicked');
+  }
+}
