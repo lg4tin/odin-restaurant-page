6c9de9e86f74172c2664721d8d1fcb6d56a02bc9
lg4tin <lg4tin@gmail.com>
2023-09-26 19:07:48 -0400

    Fix switch tab function

--- src/index.js
+++ src/index.js
@@ -21,13 +21,13 @@ contactButton.addEventListener('click', () => {
   menuSwitch(menuButton, homeButton, contactButton);
 })
 
-console.log('Hiii');
+console.log('Hiiii');
 
 
 function menuSwitch(btn1, btn2, btn3) {
   if (!btn1.classList.contains('clicked') && !btn2.classList.contains('clicked')) {
     btn3.classList.add('clicked');
-  } else if (btn1.classList.contains('clicked') || btn2classList.contains('clicked')) {
+  } else if (btn1.classList.contains('clicked') || btn2.classList.contains('clicked')) {
     btn1.classList.remove('clicked');
     btn2.classList.remove('clicked');
     btn3.classList.add('clicked');
