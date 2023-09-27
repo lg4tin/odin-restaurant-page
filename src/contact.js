b647e9bdf44687fc2115815a9f5b7010688bc763
lg4tin <lg4tin@gmail.com>
2023-09-26 20:45:27 -0400

    Finish contact page

--- src/contact.js
+++ src/contact.js
@@ -2,9 +2,27 @@ const content = document.querySelector('#content');
 
 export function displayContactPage() {
   content.innerHTML = '';
-  document.body.style = 'background: none; background-color: lightpink;';
+  content.style = 'padding-top: 20px;'
+  document.body.style = 'background: none; background-color: beige;';
 
   let header = document.createElement('h1');
   header.textContent = 'CONTACT US!';
+  header.style = 'color: black;'
   content.appendChild(header);
+
+  let infoContainer = document.createElement('div');
+  infoContainer.classList.add('menu-container');
+  content.appendChild(infoContainer);
+
+  let address = document.createElement('div');
+  address.textContent = '555 Main St Tampa FL, 33614';
+  infoContainer.appendChild(address);
+
+  let email = document.createElement('div');
+  email.textContent = 'Email us at me@bestpizzashop.com';
+  infoContainer.appendChild(email);
+
+  let phoneNum = document.createElement('div');
+  phoneNum.textContent = 'Call us at 555-555-5555';
+  infoContainer.appendChild(phoneNum);
 }
